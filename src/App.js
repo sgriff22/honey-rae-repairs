4a44ba3ef4eec7f5e7c3be40553e60458d1474be
Stacy Griffy <sgriffy2@gmail.com>
2024-01-05 14:41:49 -0600

    Added first component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
-}
+  const [count, setCount] = useState(0); //[stateVariable, setterFunction]
+
+  const handleButtonClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  return (
+    <>
+      <h1>Hello!</h1>
+      <div>This is amazing!</div>
+      <button className="btn-secondary" onClick={handleButtonClick}>
+        Click me!
+      </button>
+      <div>Count: {count}</div>
+    </>
+  );
+};
