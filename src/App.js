efd5fd91634992ffc715cb452f4bb25b49a9e1c6
Stacy Griffy <sgriffy2@gmail.com>
2024-01-15 13:11:29 -0600

    created NavBar component with styling and added routes functionality.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import { CustomerList } from "./components/customers/CustomerList";
 import { EmployeeList } from "./components/employees/EmployeeList";
+import { NavBar } from "./components/nav/NavBar";
 import { TicketList } from "./components/tickets/TicketList";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 export const App = () => {
   return (
-    <>
-      {/*<TicketList />*/}
-      {/*<CustomerList />*/}
-      <EmployeeList />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+        <Route path="employees" element={<EmployeeList />} />
+      </Route>
+    </Routes>
   );
 };
