f3f3b97c6dae6d0d683b63f3046930c503d55abd
Stacy Griffy <sgriffy2@gmail.com>
2024-01-17 12:07:15 -0600

    Implemented a form for employee to edit their profile

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,3 +1,5 @@
+import { json } from "react-router-dom";
+
 export const getAllEmployees = () => {
   return fetch("http://localhost:8088/employees?_expand=user").then((res) =>
     res.json()
@@ -8,4 +10,14 @@ export const getEmployeeById = (id) => {
   return fetch(
     `http://localhost:8088/employees?userId=${id}&_expand=user&_embed=employeeTickets`
   ).then((res) => res.json());
+};
+
+export const updateEmployee = (employee) => {
+  return fetch(`http://localhost:8088/employees/${employee.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(employee),
+  });
 };
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -7,6 +7,7 @@ import { EmployeeDetails } from "../components/employees/EmployeeDetails";
 import { CustomerList } from "../components/customers/CustomerList";
 import { CustomerDetails } from "../components/customers/CustomerDetails";
 import { useEffect, useState } from "react";
+import { EmployeeForm } from "../components/forms/EmployeeForm";
 
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({});
@@ -42,6 +43,10 @@ export const ApplicationViews = () => {
           <Route index element={<CustomerList />} />
           <Route path=":customerId" element={<CustomerDetails />} />
         </Route>
+        <Route
+          path="profile"
+          element={<EmployeeForm currentUser={currentUser} />}
+        />
       </Route>
     </Routes>
   );
